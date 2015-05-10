6c79695a5d87424ba418da620113cf38b535ee62
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2015-05-10 01:42:44 -0700

    annoying debug statements

--- bin/serve_index.py
+++ bin/serve_index.py
@@ -8,8 +8,10 @@ def index():
 
 if __name__ == '__main__':
     port = getenv("PORT")
+    print getenv("PWD")
     if port:
         port = int(port)
     else:
         port = 5000
+    print "booted up serve_index"
     app.run(port=port)
