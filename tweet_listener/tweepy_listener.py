47042e83f5d5f5b0846c723f4ad891d11a26eb3f
Sarah Tappon <sarah.tappon@gmail.com>
2015-05-10 20:43:58 -0700

    make DisruptHorn reply to people

--- tweet_listener/tweepy_listener.py
+++ tweet_listener/tweepy_listener.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 from tweepy import Stream
 from tweepy import OAuthHandler
+from tweepy import API
 from tweepy.streaming import StreamListener
 from os import getenv
+from random import randint
 import json
 import requests
 
@@ -43,15 +45,32 @@ class SparkClient:
 
 
 class TwitterListener(StreamListener):
-    def __init__(self, spark_client):
+    def __init__(self, tweepy_api, spark_client, username):
+        self.api = tweepy_api
         self.spark_client = spark_client
+        self.username = username
+
+    @staticmethod
+    def air_horn_sound():
+        return "BYOOOO%sNK" % ("R" * randint(6,25))
 
     def on_data(self, data):
         tweet = json.loads(data)
-        # FIXME: how to fix utf8 print to terminal?
-        print("twitter_listener:on_data", tweet['user']['screen_name'], tweet['text'])
-        spark_client.disrupt()
+        user = tweet['user']['screen_name']
+        if (user != self.username):
+            # FIXME: how to fix utf8 print to terminal?
+            print("twitter_listener:on_data", user, tweet['text'])
+            spark_client.disrupt()
+            # check environment variable before each reply, in case we need to disable
+            # replies in a hurry
+            if (getenv('DISRUPT_REPLY') is not None):
+                self.reply_to_tweet(user, tweet['id'])
         return(True)
+
+    def reply_to_tweet(self, user, tweet_id):
+        status = "@%s %s" % (user, self.air_horn_sound())
+        print("twitter_listener:reply_to_tweet", status, tweet_id)
+        self.api.update_status(status=status, in_reply_to_status_id=tweet_id)
 
     def on_error(self, status):
         print("twitter_listener:on_error",status)
@@ -66,5 +85,7 @@ auth.set_access_token(atoken, asecret)
 spark_client = SparkClient()
 
 # create our twitter streaming listener
-twitterStream = Stream(auth, TwitterListener(spark_client))
+api = API(auth)
+listener = TwitterListener(api, spark_client, "DisruptHorn")
+twitterStream = Stream(auth, listener)
 twitterStream.filter(track=["#disrupt"])
